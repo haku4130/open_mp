77af7f26970b9811e044cdbce35f2c2d045bf6ad
haku <haku37376@gmail.com>
2023-09-14 16:00:59 +0300

    Изменения в cmake для работы с openmp, определение версии openmp в программе

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(int argc, char** argv)
+{
+    const int count = 10000000;     ///< Number of array elements
+    const int threads = omp_get_max_threads();         ///< Number of parallel threads to use
+    const int random_seed = 920215; ///< RNG seed
+
+    int* array = 0;                 ///< The array we need to find the max in
+    int  max   = -1;                ///< The maximal element
+
+    double openmp_version = omp_get_wtime();
+    printf("OpenMP version: %f\n", openmp_version);
+
+    /* Initialize the RNG */
+    srand(random_seed);
+
+    /* Generate the random array */
+    array = (int*)malloc(count*sizeof(int));
+    for(int i=0; i<count; i++) { array[i] = rand(); }
+
+    /* Find the maximal element */
+#pragma omp parallel num_threads(threads) shared(array, count) reduction(max: max) default(none)
+    {
+#pragma omp for
+        for(int i=0; i<count; i++)
+        {
+            if(array[i] > max) { max = array[i]; };
+        }
+        printf("-%d- My lmax is: %d;\n", omp_get_thread_num(), max);
+    }
+
+    printf("======\nMax is: %d;\n", max);
+    return(0);
+}
